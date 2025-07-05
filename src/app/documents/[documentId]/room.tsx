6a5ac816c668d328f2efb467850c89c273bae2bb
Utkarsh Kumawat <utkarshkumawat77955@gmail.com>
2025-07-05 09:48:01 +0530

    feat(documents): add user fetching and avatar display in Room component
    This commit introduces a new `getUsers` function to fetch user data from Clerk, enhancing the Room component with user presence features. Additionally, a new `Avatars` component is created to display user avatars, improving the visual representation of participants in the room. The Room component is updated to utilize these features, providing a more interactive and engaging user experience.

--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -1,18 +1,61 @@
 "use client";
 
-import {ReactNode} from "react";
+import {ReactNode, useEffect, useMemo, useState} from "react";
 import {
 	LiveblocksProvider,
 	RoomProvider,
 	ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import {useParams} from "next/navigation";
-import { Loader } from "@/components/loader";
+import {Loader} from "@/components/loader";
+import {getUsers} from "./action";
+import {toast} from "sonner";
+type Users = {
+	id: string;
+	name: string;
+	avatar: string;
+};
 
 export function Room({children}: {children: ReactNode}) {
 	const params = useParams();
+	const [users, setUsers] = useState<Users[]>([]);
+
+	const fetchUsers = useMemo(
+		() => async () => {
+			try {
+				const list = await getUsers();
+				setUsers(list);
+			} catch (error) {
+				toast.error("Error fetching users");
+				console.error(error);
+			}
+		},
+		[]
+	);
+
+	useEffect(() => {
+		fetchUsers();
+	}, [fetchUsers]);
 	return (
-		<LiveblocksProvider throttle={16} authEndpoint="/api/liveblocks-auth">
+		<LiveblocksProvider
+			throttle={16}
+			authEndpoint="/api/liveblocks-auth"
+			resolveMentionSuggestions={({text}) => {
+				let filteredUsers = users;
+				if (text) {
+					filteredUsers = users.filter((user) =>
+						user.name.toLowerCase().includes(text.toLowerCase())
+					);
+				}
+				return filteredUsers.map((user) => user.id);
+			}}
+			resolveRoomsInfo={()=>[]}
+			resolveUsers={({userIds}) => {
+				return userIds.map((userId) =>
+					users.find((user) => user.id === userId)
+				);
+			}}
+		>
 			<RoomProvider id={params.documentId as string}>
 				<ClientSideSuspense fallback={<Loader label="Room loading" />}>
 					{children}
